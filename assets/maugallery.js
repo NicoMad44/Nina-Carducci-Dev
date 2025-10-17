ef4677b5a587a83cb2a2b2b0c60f5f6d6f84e5fd
NicoMad44 <nicolas.madier@gmail.com>
2025-10-17 16:21:47 +0200

    Filter button bug fix: selected filter button highlighted with different background

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -240,7 +240,8 @@
         return;
       }
       $(".active-tag").removeClass("active active-tag");
-      $(this).addClass("active-tag");
+      //$(this).addClass("active");
+      $(this).addClass("active active-tag");
 
       var tag = $(this).data("images-toggle");
 
